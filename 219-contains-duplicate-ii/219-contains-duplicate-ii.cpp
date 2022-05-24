3d95ff5b4eb8ae2021759b675ae9be141838f597
Hossam Saeed <47090776+hoskillua@users.noreply.github.com>
2022-05-25 00:24:03 +0200

    Time: 207 ms (70.25%), Space: 72.2 MB (89.78%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int, vector<int>> Freq;
-        vector<int> Diff;
+        unordered_set<int> window;
         int n = nums.size();
-        for (int i = 0; i < n; i++)
-            Freq[nums[i]].push_back(i);
-        for(auto f: Freq)
-            for(int i = 0; i+1 < f.second.size(); i++)
-                Diff.push_back(f.second[i+1] - f.second[i]);
-        if(Diff.size() && *min_element(Diff.begin(), Diff.end()) <= k)
-            return true;
+        for(int i=0;i<n;i++)
+        {
+            if(window.count(nums[i])) return true;
+            window.insert(nums[i]);
+            if(window.size()>k)
+                window.erase(nums[i-k]);
+        }
         return false;
     }
 };
