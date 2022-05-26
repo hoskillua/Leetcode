098c55883a7edb94880d0ad61eda7c0ea532a2b2
Hossam Saeed <47090776+hoskillua@users.noreply.github.com>
2022-05-26 20:17:21 +0200

    Time: 15 ms (72.64%), Space: 10.4 MB (59.68%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        vector<int> twoSum(vector<int>& nums, int target) {
+                int n = nums.size();
         vector<pair<int,int>> numsI(n);
         for(int i = 0; i < n; i++)
         {
             numsI[i] = make_pair(nums[i], i);
         }
         sort(numsI.begin(), numsI.end());
-        for(int i = 0; i < n; i++)
+        int i = 0, j = n - 1;
+        while(i < j)
         {
-            // get numsI[j].first = target - numsI[i].first by binary search
-            int j = binarySearch(numsI, target - numsI[i].first, i + 1, n - 1);
-            if(j < n && numsI[j].first == target - numsI[i].first)
-                return vector<int>{numsI[i].second, numsI[j].second};
+            int sum = numsI[i].first + numsI[j].first;
+            if(sum == target)
+            {
+                
+                return {(numsI[i].second), (numsI[j].second)};
+            }
+            else if(sum < target)
+                i++;
+            else
+                j--;
         }
         return {0,0};
     }
-
-    int binarySearch(vector<pair<int,int>>& numsI, int target, int left, int right)
-    {
-        while(left <= right)
-        {
-            int mid = left + (right - left) / 2;
-            if(numsI[mid].first == target)
-                return mid;
-            else if(numsI[mid].first < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return left;
-    }
+        
 };
