ec96fa7357bb395d18de169730e054352d51af2c
Hossam Saeed <47090776+hoskillua@users.noreply.github.com>
2022-05-27 22:43:08 +0200

    Time: 23 ms (5.52%), Space: 26.6 MB (85.50%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -3,9 +3,9 @@ public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
         vector <double> D = {distSq(p1,p2), distSq(p1,p3), distSq(p1,p4), distSq(p3,p2), distSq(p4,p2), distSq(p3,p4)};
         sort(D.begin(), D.end());
-        return !doubleEq(D[0], 0) && doubleEq(D[0], D[1]) 
+        return doubleEq(D[0], D[1]) 
             && doubleEq(D[2], D[1]) && doubleEq(D[2], D[3])
-            && doubleEq(D[5], D[4]);
+            && doubleEq(D[5], D[4]) && !doubleEq(D[0], 0) ;
 
     }
     inline double distSq(vector<int>& p1, vector<int>& p2)
