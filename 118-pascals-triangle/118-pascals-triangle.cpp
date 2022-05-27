ee493915230eddb3a4d1357ba00284e3f1e417e4
Hossam Saeed <47090776+hoskillua@users.noreply.github.com>
2022-05-27 21:52:39 +0200

    Time: 6 ms (6.93%), Space: 6.3 MB (91.11%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>> P(n);
         P[0].push_back(1);
-        for (int i = 1;  i < n; i++)
+        for (char i = 1;  i < n; i++)
         {
             P[i].push_back(1);
-            for(int j = 1; j < i; j++)
+            for(char j = 1; j < i; j++)
             {
                 P[i].push_back(P[i-1][j-1] + P[i-1][j]);
             }
